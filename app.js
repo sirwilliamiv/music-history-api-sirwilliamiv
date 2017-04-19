9f6e5981b15885eae47b4682d7eea6a5b9d4d01b
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-19 11:05:53 -0500

    required routes and set up middleware on appjs, put in catch all for 404 errors and middleware to handle errors

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const bodyParser = require('body-parser');
 let app = express();
 
 // <Include the router index file>
+let router = require('routes/index');
 // <Setup your routes middleware>
+app.use(bodyParser.json());
+app.use('/api/v1/', routes);
 // <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
+app.use( (req,res,next) => {
+  let err = new Error('Not Found,Dude');
+  err.status = 404;
+  next(err);
+})
+// <Handle any errors that occur in the routing with error handlers defined at the bottom of our
 // middleware stack>
+app.use( (err,req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
