cda6c9987866ec8a70318ced7eac23840b699aaf
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-19 10:39:31 -0500

    added methods to songCtrl for getSong, addSong,getAllSongs and deleteSingleSong

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -1,5 +1,52 @@
 'use strict'
 
 // <require a song model>
+const Song = require('../models/song')
 // < use model methods for getting all songs and one song then send the response back with the data>
+//get one song
+module.exports.getSong = ({params: { id } },res,next) => {
+  Song.getSingleSong(id)
+  .then( (song) => {
+    res.status(200).json(song);
+  })
+  .catch( (error) => {
+    console.log('error in songctrl for getsong', error)
+    next(error)
+  })
+}
+//get all songs
+module.exports.getSong = (req,res,next) => {
+  Song.getAllSongs()
+  .then( (songs) => {
+    res.status(200).json(songs);
+  })
+  .catch( (error) => {
+    console.log('error in songctrl for getsong', error)
+    next(error)
+  })
+}
+//adding
+module.exports.addSong = ({ body }, res, next) => {
+Song.forge(body)
+.save()
+.then( ()=> res.status(201).json({msg:"A Song was nicely posted to the DB"})
+  .catch( (error) => {
+    console.log('error in addSong in songctrl', error)
+    next(error)
+  })
+})
+}
+//delete
+module.exports.deleteSingleSong = ( { params: { id } }, res, next) => {
+  Song.forge({id})
+  .destroy()
+  .then( (song) => {
+    res.status(200).json(song)
+  })
+  .catch((error)=> {
+    console.log("error in deleteSingleSong in song ctrl", error)
+    next(error);
+  })
+}
+//editing
 // <stretch goal: methods for adding, deleting, editing a song>
--- db/database.js
+++ db/database.js
@@ -3,6 +3,8 @@ const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment]
 const knex = require('knex')(config);
 const bookshelf = require('bookshelf')(knex);
+
+
 module.exports= {knex, bookshelf}
 // I have already run 'knex init' at the root of this project to create the knexfile.js.
 // The music history database lives right here as a document in this project, so note
