5cdaeb3dbda9bd32f7cc0d672effe49ea57b5ce3
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-19 10:12:32 -0500

    configured database for bookshelf and knex, wrote two static methods in the songs js model to get single song and all songs

--- models/song.js
+++ models/song.js
@@ -1,8 +1,40 @@
 'use strict'
+const  { bookshelf } = require('../musichistory.db');
 
-// <Define a model using bookshelf that describes a song object, as well as 
-// static methods for getting one or all songs from the db>
+// <Define a model using bookshelf that describes a song object
+const Song = bookshelf.Model.extend({
+  tableName: 'Song',
 
-// When adding the 'table' property use your old friend DB Browser for SQLite to 
-// open up the musichistory db and inspect the tables. Add the appropriate table name for 
+  getAllSongs: ()=> {
+    return this.forge()
+    .fetchAll()
+    .then( (songs) => {
+      return songs
+    })
+    .catch( (error) => {
+      console.log("error from getAllSongs", error)
+      return error
+    })
+  },
+  // static methods for getting one or all songs from the db>
+  getSingleSong: (id) => {
+    return this.forge({id})
+    .fetch()
+    .then( (song) => {
+      return song;
+    })
+    .catch( (error) => {
+      console.log("error from getSingleSong", error)
+      return error;
+    })
+
+  }
+})
+
+
+
+
+// When adding the 'table' property use your old friend DB Browser for SQLite to
+// open up the musichistory db and inspect the tables. Add the appropriate table name for
 // interfacing with the songs collection
+model.exports = bookshelf.model('Song', Song)
